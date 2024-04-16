f646def10ab53aa93abef7353af5486b35110600
Sivaranjini R S <shyv3112@gmail.com>
2024-04-16 15:29:20 +0530

    Json

--- client/src/api.js
+++ client/src/api.js
@@ -1,35 +1,14 @@
-exports.handler = async (event, context) => {
-    if (event.httpMethod === 'GET') {
-        try {
-            const data = require('./db.json'); // Process the GET request as needed
-            return {
-                statusCode: 200,
-                body: JSON.stringify(data),
-            };
-        } catch (error) {
-            return {
-                statusCode: 500,
-                body: JSON.stringify({ error: 'Failed to process GET request' }),
-            };
-        }
-    }
-};
+const headers = new Headers();
+headers.append('Authorization', 'Bearer SOME-VALUE');
 
-exports.handler = async (event, context) => {
-    if (event.httpMethod === 'POST') {
-        try {
-            const requestBody = JSON.parse(event.body); // Parse incoming JSON payload
-            // Save data to a database or perform other necessary operations
-            // ...
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ message: 'POST request processed successfully' }),
-            };
-        } catch (error) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error: 'Failed to process POST request' }),
-            };
-        }
-    }
-};
+fetch('https://authinmern.free.beeceptor.com', {
+    method: 'GET',
+    headers: headers,
+  })
+  .then(response => response.text())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
