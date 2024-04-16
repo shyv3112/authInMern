f705c7ed80372f7619fe5f8d8cb130fb3317b74a
Sivaranjini R S <shyv3112@gmail.com>
2024-04-16 12:05:59 +0530

    Added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
+const { default: mongoose } = require("mongoose");
 
 // database connection
 connection();
@@ -16,6 +17,11 @@ app.use(cors());
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+
+const dbOptions = {useNewUrlParser: true, useUnifiedTopology: true}
+mongoose.connect(process.env.DB, dbOptions)
+.then (() => console.log('DB Connected'))
+.catch (err => console.log(err))
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
